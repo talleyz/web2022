a50f9b00e0b0931c96c51bc20f57ecb36af65c8a
Zachary Talley <zvta222@g.uky.edu>
2022-04-24 20:33:52 -0400

    java intro

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-+
+// Start dark mode if it's after 6:59:59pm
+if (new Date().getHours() > 18) {
+  //alert(new Date().getHours());
+  const html = document.querySelector('html');
+  html.style.backgroundColor = 'black';
+  html.style.color = 'white';
+}
+
+// Fuction- Start dark mode by 6pm
+
+
+if (new Date().getHours() > 18) {
+  const wrapperBody = document.getElementsByClassName('wrapper');
+  wrapperBody[0].style.backgroundColor = 'black';
+  wrapperBody[0].style.color = 'white';
+}
+
+if (new Date().getHours() > 18) {
+  const secondaryBody = document.getElementById('secondary');
+  secondaryBody.style.backgroundColor = 'black';
+  secondaryBody.style.color = 'white';
+}
